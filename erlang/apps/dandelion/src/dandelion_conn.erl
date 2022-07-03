853f1191308c36dd76bd226d73df1acadacead2d
Fred Hebert <mononcqc@ferd.ca>
2022-07-03 13:44:50 -0400

    Setting things in place for relup generation
    - Include the relup plugin from source (more up to date)
    - Drop the prod profile for releases and always run as prod (to work
      better with the plugin)
    - Call release before tar to generate some metadata files
    - Fixing version munging for tags in gh actions
    - Optionally generate relup files based on versions and existing
      manifest data

--- erlang/apps/dandelion/src/dandelion_conn.erl
+++ erlang/apps/dandelion/src/dandelion_conn.erl
@@ -7,6 +7,7 @@
 -behaviour(gen_server).
 
 -define(CONTINUE_WAIT, 500).
+-define(DISPLAY_DELAY, timer:seconds(2)).
 
 -export([start_link/1]).
 -export([init/1, handle_continue/2, handle_call/3, handle_cast/2, handle_info/2]).
@@ -22,6 +23,7 @@ handle_continue(wait_control, State=#{sock := Sock}) ->
     %% The wait shouldn't be long
     receive
         {ready, Sock} ->
+            display_event(),
             inet:setopts(Sock, [{active, once}]),
             {noreply, State}
     after ?CONTINUE_WAIT ->
@@ -34,11 +36,23 @@ handle_call(_Call, _From, State) ->
 handle_cast(_Cast, State) ->
     {noreply, State}.
 
+handle_info(display, State=#{sock := Sock}) ->
+    Str = "\n"
+          "   @\n"
+          " \ |\n"
+          "__\!/__\n\n",
+    gen_tcp:send(Sock, Str),
+    display_event(),
+    {noreply, State};
 handle_info({tcp, Sock, _Msg}, State=#{sock := Sock}) ->
     inet:setopts(Sock, [{active, once}]),
-    gen_tcp:send(Sock, ".\n"),
+    {ok, Vsn} = application:get_key(dandelion, vsn),
+    gen_tcp:send(Sock, "vsn: " ++ Vsn ++ "\n"),
     {noreply, State};
 handle_info({tcp_error, _Sock, Reason}, State) ->
     {stop, {shutdown, {sock, Reason}}, State};
 handle_info({tcp_closed, _Sock}, State) ->
     {stop, {shutdown, {sock, closed}}, State}.
+
+display_event() ->
+    erlang:send_after(?DISPLAY_DELAY, self(), display).
